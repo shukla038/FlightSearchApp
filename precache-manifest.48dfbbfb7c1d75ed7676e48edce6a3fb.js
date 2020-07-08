8e105db6fb5d17679647d423f0e13bb823cd8da2
vastdreams258 <servify@Sl-sp-lap-0331.local>
2020-07-08 13:11:20 +0530

    Updates

---
+++ precache-manifest.48dfbbfb7c1d75ed7676e48edce6a3fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5fa3aa92e530ac82fd4505332143e2",
+    "url": "/FlightSearchApp/index.html"
+  },
+  {
+    "revision": "a63caea1df2bc64d3545",
+    "url": "/FlightSearchApp/static/css/4.f5244aec.chunk.css"
+  },
+  {
+    "revision": "cd474f92afe2e1733bfa",
+    "url": "/FlightSearchApp/static/css/main.45ee8331.chunk.css"
+  },
+  {
+    "revision": "5aeb2f473a0d42078df2",
+    "url": "/FlightSearchApp/static/js/2.5fdeda63.chunk.js"
+  },
+  {
+    "revision": "f7633ba73ef876000000",
+    "url": "/FlightSearchApp/static/js/3.95a5ed09.chunk.js"
+  },
+  {
+    "revision": "a63caea1df2bc64d3545",
+    "url": "/FlightSearchApp/static/js/4.931c5ca5.chunk.js"
+  },
+  {
+    "revision": "cd474f92afe2e1733bfa",
+    "url": "/FlightSearchApp/static/js/main.7ee4dcfa.chunk.js"
+  },
+  {
+    "revision": "24254c9cae583c73277e",
+    "url": "/FlightSearchApp/static/js/runtime~main.541516b4.js"
+  },
+  {
+    "revision": "ece3a8aeb4c5f493aeb0cc0f4f7d1cdf",
+    "url": "/FlightSearchApp/static/media/airplane.ece3a8ae.svg"
+  }
+]);
